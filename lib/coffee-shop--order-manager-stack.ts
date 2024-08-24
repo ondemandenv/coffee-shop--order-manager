58b1cbb4de317b23dc557f27601cc1c1edb8b18e
jj <jj@ondemandenv.dev>
2024-08-23 22:24:59 -0400

    init +

--- lib/coffee-shop--order-manager-stack.ts
+++ lib/coffee-shop--order-manager-stack.ts
@@ -1,16 +1,59 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import {
+    CoffeeShopOrderManagerEnver
+} from "@ondemandenv/odmd-contracts/lib/repos/coffee-shop/coffee-shop-order-manager-cdk";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {ContractsEnverCdk} from "@ondemandenv/odmd-contracts/lib/odmd-model/contracts-enver-cdk";
+import {EventBus, Rule} from "aws-cdk-lib/aws-events";
+import {OndemandContracts} from "@ondemandenv/odmd-contracts";
+import {
+    CoffeeShopOrderProcessorEnver
+} from "@ondemandenv/odmd-contracts/lib/repos/coffee-shop/coffee-shop-order-processor-cdk";
+import {AttributeType, BillingMode, Table} from "aws-cdk-lib/aws-dynamodb";
+import {aws_events_targets, RemovalPolicy} from "aws-cdk-lib";
+
 
 export class CoffeeShopOrderManagerStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, enver: ContractsEnverCdk, props?: cdk.StackProps) {
+        const id = enver.getRevStackNames()[0];
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const myEnver = enver as CoffeeShopOrderProcessorEnver
+        const eventBus = EventBus.fromEventBusName(this, 'eventBus', myEnver.eventBus.getSharedValue(this))
+        const source = myEnver.eventSrc.getSharedValue(this) as string
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CoffeeShopOrderManagerQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // const configTable = Table.fromTableName(this, 'configTable', myEnver.configTableName.getSharedValue(this))
+        // const countingTable = Table.fromTableName(this, 'countTableName', myEnver.countTableName.getSharedValue(this))
+
+
+        // e.eventBus.getSharedValue(this)
+
+        const orderTable = new Table(this, 'orderTable', {
+            partitionKey: {name: 'PK', type: AttributeType.STRING},
+            sortKey: {name: 'SK', type: AttributeType.STRING},
+            billingMode: BillingMode.PAY_PER_REQUEST,
+            // billing: Billing.onDemand(),
+            removalPolicy: RemovalPolicy.DESTROY
+        });
+
+        const onWorkflowStartedFunc = new NodejsFunction(this, 'onWorkflowStarted-func', {
+            entry: __dirname + '/onWorkflowStarted/index.ts',
+            environment: {
+                orderTableName: orderTable.tableName
+            }
+        });
+        new Rule(this, 'onWorkflowStarted-rule', {
+            eventBus, eventPattern: {
+                source: [source],
+                detailType: [OndemandContracts.inst.coffeeShopOrderProcessorCdk.WORKFLOW_STARTED]
+            },
+            targets: [new aws_events_targets.LambdaFunction(
+                onWorkflowStartedFunc
+            )]
+        })
+        orderTable.grantFullAccess(onWorkflowStartedFunc)
+
+
+    }
 }
---
+++ lib/onWorkflowStarted/index.ts
@@ -0,0 +1,52 @@
+import {EventBridgeEvent} from "aws-lambda/trigger/eventbridge";
+import {Context} from "aws-lambda";
+import {DynamoDBDocument} from "@aws-sdk/lib-dynamodb"
+import {DynamoDB} from "@aws-sdk/client-dynamodb"
+import {SFN} from "@aws-sdk/client-sfn"
+import {UpdateCommandInput} from "@aws-sdk/lib-dynamodb/dist-types/commands/UpdateCommand";
+
+
+export async function handler(event: EventBridgeEvent<any, any>, context: Context): Promise<void> {
+
+    console.log(`>>>event
+    ${JSON.stringify(event, null, 4)}
+----event----context----
+    ${JSON.stringify(context, null, 4)}
+<<<context`)
+
+    const documentClient = DynamoDBDocument.from(new DynamoDB())
+    const stepFunctions = new SFN()
+
+
+    const params = {
+        TableName: process.env.orderTableName,
+        Key: {
+            PK: 'orders',
+            SK: event.detail.orderId,
+        },
+        UpdateExpression: "set drinkOrder = :drinkOrder, TS = :TS",
+        ConditionExpression: "#userId = :userId",
+        ExpressionAttributeNames: {
+            "#userId": "USERID"
+        },
+        ExpressionAttributeValues: {
+            ":drinkOrder": event.detail.drink,
+            ":userId": event.detail.userId,
+            ":TS": Date.now()
+        },
+        ReturnValues: "ALL_NEW"
+    } as UpdateCommandInput
+
+    console.log(params)
+    const result = await documentClient.update(params)
+    console.log(result)
+
+    // Update Step Functions workflow
+    const sfnParams = {
+        taskToken: result.Attributes!.TaskToken,
+        output: JSON.stringify({'orderId': event.detail.orderId})
+    }
+    console.log({sfnParams})
+    const sfnResult = await stepFunctions.sendTaskSuccess(sfnParams)
+    console.log({sfnResult})
+}
